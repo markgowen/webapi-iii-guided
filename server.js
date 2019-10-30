849dce4e9f186b133d30a0a105ef146d55904ff7
Mark Gowen <mark.gowen78@gmail.com>
2019-10-30 10:05:52 -0700

    1st exercise

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+const Logger = (req, res, next) => {
+  const { url, method } = req;
+  console.log(`Logged ${url} and ${method}`);
+  next();
+};
 
+server.use(express.json());
+server.use(helmet());
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+  const nameInsert = req.name ? ` ${req.name}` : '';
 
   res.send(`
     <h2>Lambda Hubs API</h2>
