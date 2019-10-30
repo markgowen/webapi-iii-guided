0db4863bd4fc6a43b4afe84ce535d2d3ce147c47
Mark Gowen <mark.gowen78@gmail.com>
2019-10-30 10:34:11 -0700

    2nd exercise

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
+const morgan = require('morgan');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-const Logger = (req, res, next) => {
-  const { url, method } = req;
-  console.log(`Logged ${url} and ${method}`);
-  next();
-};
+function Logger(req, res, next) {
+  console.log(
+    ` The Logger: [${new Date().toISOString()}] ${req.method} to ${req.url}`
+  );
 
+  next();
+}
+
+function gateKeeper(req, res, next) {
+  const password = req.headers.password || '';
+  if (!password.toLowerCase) {
+    res.status(401).json({ message: `please provide a password` });
+    return;
+  }
+  if (password.toLowerCase() === 'mellon') {
+    next();
+  } else {
+    res.status(400).json({ you: 'cannot pass!!' });
+  }
+}
+
+// Global Middleware
+server.use(gateKeeper);
 server.use(express.json());
 server.use(helmet());
+server.use(logger);
+server.use(morgan('dev'));
+
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
